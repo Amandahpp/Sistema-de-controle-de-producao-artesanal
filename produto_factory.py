d7942411eafd4d92868bc01409862d206f9c495b
Amandahpp <amandaandreis32@gmail.com>
2026-06-24 15:16:22 -0300

    Delete produto_factory.py

--- produto_factory.py
+++
@@ -1,6 +0,0 @@
-from produtocroche import Produto
-
-class ProdutoFactory:
-    @staticmethod
-    def criar_produto(id,nome,descricao, preco_venda,tempo_producao):
-          return Produto(id,nome,descricao,preco_venda,tempo_producao)
